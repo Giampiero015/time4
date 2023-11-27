1b7849594acb0e4fd9bdc56eff2e6d09e78dd165
Giampiero015 <150815405+Giampiero015@users.noreply.github.com>
2023-11-27 11:09:42 +0100

    Add files via upload

---
+++ es4.js
@@ -0,0 +1,12 @@
+function Dado (){
+    return Math.floor(Math.random()*(6)+1);
+}
+let dado1=Dado();
+let dado2=Dado();
+setTimeout(() => {
+    console.log(`Lancio dei dadi: Dado 1= ${dado1} Dado 2= ${dado2}`);    
+}, 2000);
+
+setTimeout(() => {
+    console.log("Lancio dei dadi in corso: ");
+}, 1000);
